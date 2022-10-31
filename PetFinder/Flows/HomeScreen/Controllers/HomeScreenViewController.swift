8fd0e1837eb649040e6171fc33e2feaef303859b
Mihai Alexandru <mh.alex93@gmail.com>
2022-10-31 14:19:42 +0200

    - Updated home controller

--- PetFinder/Flows/HomeScreen/Controllers/HomeScreenViewController.swift
+++ PetFinder/Flows/HomeScreen/Controllers/HomeScreenViewController.swift
@@ -7,8 +7,30 @@
 
 import Foundation
 import UIKit
+import Alamofire
 
 class HomeScreenViewController: BaseViewController {
     
+    @IBOutlet weak var petsTableView: UITableView!
     var coordinator: MainCoordinator?
+    
+    override func viewDidLoad() {
+        self.petsTableView.delegate = self
+        super.viewDidLoad()
+    }
+}
+
+extension HomeScreenViewController: UITableViewDelegate {
+    
+}
+
+extension HomeScreenViewController: UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        0
+        
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        UITableViewCell()
+    }
 }
